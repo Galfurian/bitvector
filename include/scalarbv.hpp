a1dd26bb2fdc80c4788e4381fb72ae063ff1931f
Enrico Fraccaroli <enrico.fraccaroli@univr.it>
2021-07-21 16:41:40 +0200

    Add a bitvector class that uses a scalar instead of an array of booleans.

---
+++ include/scalarbv.hpp
@@ -0,0 +1,241 @@
+/*! \file
+ * # Definition of the classes for managing categorical data arrays.
+ *
+ * This header file defines classes allowing to store sampled input points, the corresponding outputs and derivatives.
+ *
+ * \copyright (c) 2020 SYDELITY b.v., all rights reserved.
+ */
+
+#pragma once
+
+#include <cassert>
+#include <iostream>
+
+#define bit_set(V, B)          ((V) | (1ULL << (B)))   ///< Sets the given bit.
+#define bit_clear(V, B)        ((V) & ~(1ULL << (B)))  ///< Clears the given bit.
+#define bit_flip(V, B)         ((V) ^ (1ULL << (B)))   ///< Flips the given bit.
+#define bit_set_assign(V, B)   ((V) |= (1ULL << (B)))  ///< Sets the given bit, permanently.
+#define bit_clear_assign(V, B) ((V) &= ~(1ULL << (B))) ///< Clears the given bit, permanently.
+#define bit_flip_assign(V, B)  ((V) ^= (1ULL << (B)))  ///< Flips the given bit, permanently.
+#define bit_check(V, B)        ((V) & (1ULL << (B)))   ///< Checks if the given bit is 1.
+
+#define bit_set_from(V, B, S) ((S) ? bit_set_assign(V, B) : bit_clear_assign(V, B))
+
+#define bitmask_set(V, M)          ((V) | (M))   ///< Sets the bits identified by the mask.
+#define bitmask_clear(V, M)        ((V) & ~(M))  ///< Clears the bits identified by the mask.
+#define bitmask_flip(V, M)         ((V) ^ (M))   ///< Flips the bits identified by the mask.
+#define bitmask_set_assign(V, M)   ((V) |= (M))  ///< Sets the bits identified by the mask, permanently.
+#define bitmask_clear_assign(V, M) ((V) &= ~(M)) ///< Clears the bits identified by the mask, permanently.
+#define bitmask_flip_assign(V, M)  ((V) ^= (M))  ///< Flips the bits identified by the mask, permanently.
+#define bitmask_check(V, M)        ((V) & (M))   ///< Checks if the bits identified by the mask are all 1.
+
+class ScalarBitVector {
+public:
+    using size_type_t  = unsigned long long;
+    using value_type_t = unsigned long long;
+
+    value_type_t value = 0ULL;
+
+    ScalarBitVector()
+        : value(0)
+    {
+    }
+
+    ScalarBitVector(value_type_t _value)
+        : value(_value)
+    {
+    }
+
+    const char *to_string() const
+    {
+        static char str[65] = { 0 };
+        for (size_type_t i = 0; i < 64ULL; ++i)
+            str[i] = bit_check(value, 64ULL - 1 - i) ? '1' : '0';
+        return str;
+    }
+
+    value_type_t to_number() const
+    {
+        return value;
+    }
+
+    value_type_t to_ullong() const
+    {
+        return value;
+    }
+
+    /// @brief Flips every bit.
+    inline auto &flip()
+    {
+        value = ~value;
+        return (*this);
+    }
+
+    /// @brief Flips a given bit.
+    inline ScalarBitVector &flip(size_type_t position)
+    {
+        assert(position < 64ULL);
+        bit_flip_assign(value, position);
+        return (*this);
+    }
+
+    /// @brief Flips a given bit.
+    inline ScalarBitVector &set(size_type_t position)
+    {
+        assert(position < 64ULL);
+        bit_set_assign(value, position);
+        return (*this);
+    }
+
+    /// @brief Flips a given bit.
+    inline ScalarBitVector &clear(size_type_t position)
+    {
+        assert(position < 64ULL);
+        bit_clear_assign(value, position);
+        return (*this);
+    }
+
+    /// @brief Returns the size in bit of the value.
+    inline size_t size() const
+    {
+        return 64ULL;
+    }
+
+    /// @brief Tests whether all the bits are on.
+    inline bool all() const
+    {
+        return ((value + 1) & value) == 0;
+    }
+
+    /// @brief Tests whether any of the bits are on.
+    inline bool any() const
+    {
+        return value != 0;
+    }
+
+    /// @brief Tests whether none of the bits are on.
+    inline bool none() const
+    {
+        return value == 0;
+    }
+
+    /// @brief Returns the number of bits which are set.
+    inline size_type_t count() const
+    {
+        size_type_t result = 0;
+        for (size_type_t i = 0; i < 64ULL; ++i)
+            result += bit_check(value, i);
+        return result;
+    }
+
+    inline bool operator[](size_type_t position) const
+    {
+        assert(position < 64ULL);
+        return bit_check(value, position);
+    }
+
+    inline bool operator==(const ScalarBitVector &rhs) const
+    {
+        return value == rhs.value;
+    }
+
+    inline bool operator!=(const ScalarBitVector &rhs) const
+    {
+        return value != rhs.value;
+    }
+
+    inline bool operator<(const ScalarBitVector &rhs) const
+    {
+        return value < rhs.value;
+    }
+
+    inline bool operator>(const ScalarBitVector &rhs) const
+    {
+        return value > rhs.value;
+    }
+
+    inline bool operator<=(const ScalarBitVector &rhs) const
+    {
+        return value <= rhs.value;
+    }
+
+    inline bool operator>=(const ScalarBitVector &rhs) const
+    {
+        return value >= rhs.value;
+    }
+
+    inline ScalarBitVector operator+(const ScalarBitVector &rhs) const
+    {
+        ScalarBitVector result(value);
+        result.value += rhs.value;
+        return result;
+    }
+
+    inline ScalarBitVector operator-(const ScalarBitVector &rhs) const
+    {
+        ScalarBitVector result(value);
+        result.value -= rhs.value;
+        return result;
+    }
+
+    inline ScalarBitVector operator*(const ScalarBitVector &rhs) const
+    {
+        ScalarBitVector result(value);
+        result.value *= rhs.value;
+        return result;
+    }
+
+    inline ScalarBitVector operator/(const ScalarBitVector &rhs) const
+    {
+        ScalarBitVector result(value);
+        result.value /= rhs.value;
+        return result;
+    }
+
+    inline ScalarBitVector operator&(const ScalarBitVector &rhs) const
+    {
+        ScalarBitVector result(value);
+        result.value &= rhs.value;
+        return result;
+    }
+
+    inline ScalarBitVector operator^(const ScalarBitVector &rhs) const
+    {
+        ScalarBitVector result(value);
+        result.value ^= rhs.value;
+        return result;
+    }
+
+    inline ScalarBitVector operator|(const ScalarBitVector &rhs) const
+    {
+        ScalarBitVector result(value);
+        result.value |= rhs.value;
+        return result;
+    }
+
+    inline ScalarBitVector operator~() const
+    {
+        ScalarBitVector result(value);
+        result.value = ~result.value;
+        return result;
+    }
+
+    inline ScalarBitVector operator!() const
+    {
+        ScalarBitVector result(value);
+        result.value = !result.value;
+        return result;
+    }
+
+    inline ScalarBitVector operator-()
+    {
+        return ScalarBitVector(-value);
+    }
+};
+
+/// @brief Overrides the output stream operator.
+inline std::ostream &operator<<(std::ostream &lhs, ScalarBitVector const &rhs)
+{
+    lhs << rhs.to_number();
+    return lhs;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,13 @@
 #include <vector>
 
 #include "bitvector.hpp"
+#include "scalarbv.hpp"
 
-#define TEST_OP(op)                                                             \
+#define TEST_BINARY_OP(op)                                                      \
     std::cout << "== Testing " #op " " << std::string(25, '=') << "\n";         \
     for (size_t it = 0; it < inputs.size(); ++it) {                             \
         for (size_t it2 = 0; it2 < inputs.size(); ++it2) {                      \
-            auto a = inputs[it], b = inputs[it2];                               \
+            auto &a = inputs[it], &b = inputs[it2];                             \
             std::cout                                                           \
                 << std::setw(12) << a.bitVector << " " #op " "                  \
                 << std::setw(12) << b.bitVector << " = "                        \
@@ -20,29 +21,81 @@
         }                                                                       \
     }
 
+#define TEST_UNARY_OP(op)                                                          \
+    std::cout << "== Testing " #op " " << std::string(25, '=') << "\n";            \
+    for (const auto &input : inputs) {                                             \
+        std::cout << " " #op << std::setw(12) << input.bitVector << " = "          \
+                  << std::setw(12) << std::right << (op input.bitVector) << " == " \
+                  << std::setw(12) << std::left << (op input.original)             \
+                  << ((op input.bitVector) == (op input.original))                 \
+                  << "\n";                                                         \
+    }
+
 template <long unsigned int N>
-struct test_pair_t {
+struct test_bv_pair_t {
     BitVector<N> bitVector;
     long unsigned int original;
-
-    test_pair_t(long unsigned int _original)
+    test_bv_pair_t(long unsigned int _original)
         : bitVector(_original),
           original(_original)
     {
     }
 };
 
-int main()
+struct test_sbv_pair_t {
+    ScalarBitVector bitVector;
+    ScalarBitVector::value_type_t original;
+    test_sbv_pair_t(long unsigned int _original)
+        : bitVector(_original),
+          original(_original)
+    {
+    }
+};
+
+void test_bitvector()
 {
-    std::vector<test_pair_t<32>> inputs;
-    inputs.emplace_back(test_pair_t<32>(6));
-    inputs.emplace_back(test_pair_t<32>(4));
-    inputs.emplace_back(test_pair_t<32>(2));
-    inputs.emplace_back(test_pair_t<32>(25));
-    inputs.emplace_back(test_pair_t<32>(321));
-    inputs.emplace_back(test_pair_t<32>(76));
-    inputs.emplace_back(test_pair_t<32>(1023));
-    inputs.emplace_back(test_pair_t<32>(65));
+    std::vector<test_bv_pair_t<32>> inputs;
+    inputs.emplace_back(test_bv_pair_t<32>(6));
+    inputs.emplace_back(test_bv_pair_t<32>(4));
+    inputs.emplace_back(test_bv_pair_t<32>(2));
+    inputs.emplace_back(test_bv_pair_t<32>(25));
+    inputs.emplace_back(test_bv_pair_t<32>(321));
+    inputs.emplace_back(test_bv_pair_t<32>(76));
+    inputs.emplace_back(test_bv_pair_t<32>(1023));
+    inputs.emplace_back(test_bv_pair_t<32>(65));
+
+    std::cout << "== INPUTS " << std::string(25, '=') << "\n";
+    for (size_t it = 0; it < inputs.size(); ++it) {
+        std::cout << "op" << it
+                  << " [" << inputs[it].bitVector.to_string() << "] "
+                  << inputs[it].bitVector.to_number()
+                  << " (" << inputs[it].original << ")\n";
+    }
+    std::cout << std::string(35, '=') << "\n";
+    TEST_BINARY_OP(+)
+    TEST_BINARY_OP(-)
+    TEST_BINARY_OP(*)
+    TEST_BINARY_OP(/)
+
+    TEST_BINARY_OP(<)
+    TEST_BINARY_OP(<=)
+    TEST_BINARY_OP(>)
+    TEST_BINARY_OP(>=)
+    TEST_BINARY_OP(==)
+    TEST_BINARY_OP(!=)
+}
+
+void test_scalar_bitvector()
+{
+    std::vector<test_sbv_pair_t> inputs;
+    inputs.emplace_back(test_sbv_pair_t(6));
+    inputs.emplace_back(test_sbv_pair_t(4));
+    inputs.emplace_back(test_sbv_pair_t(2));
+    inputs.emplace_back(test_sbv_pair_t(25));
+    inputs.emplace_back(test_sbv_pair_t(321));
+    inputs.emplace_back(test_sbv_pair_t(76));
+    inputs.emplace_back(test_sbv_pair_t(1023));
+    inputs.emplace_back(test_sbv_pair_t(65));
 
     std::cout << "== INPUTS " << std::string(25, '=') << "\n";
     for (size_t it = 0; it < inputs.size(); ++it) {
@@ -53,16 +106,25 @@ int main()
     }
     std::cout << std::string(35, '=') << "\n";
 
-    TEST_OP(+)
-    TEST_OP(-)
-    TEST_OP(*)
-    TEST_OP(/)
+    TEST_BINARY_OP(+)
+    TEST_BINARY_OP(-)
+    TEST_BINARY_OP(*)
+    TEST_BINARY_OP(/)
 
-    TEST_OP(<)
-    TEST_OP(<=)
-    TEST_OP(>)
-    TEST_OP(>=)
-    TEST_OP(==)
-    TEST_OP(!=)
+    TEST_BINARY_OP(<)
+    TEST_BINARY_OP(<=)
+    TEST_BINARY_OP(>)
+    TEST_BINARY_OP(>=)
+    TEST_BINARY_OP(==)
+    TEST_BINARY_OP(!=)
+    TEST_BINARY_OP(|)
+    TEST_UNARY_OP(!)
+    TEST_UNARY_OP(~)
+}
+
+int main()
+{
+    test_bitvector();
+    test_scalar_bitvector();
     return 0;
 }
