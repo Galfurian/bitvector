9a86fdf86c8f128792afc539b608bee5117fbfec
Enrico Fraccaroli (Galfurian) <enry.frak@gmail.com>
2025-02-17 10:27:51 -0500

    Update version.

--- include/bvlib/bitvector.hpp
+++ include/bvlib/bitvector.hpp
@@ -16,7 +16,7 @@
 
 enum : unsigned char {
     BVLIB_MAJOR_VERSION = 1, ///< Major version of the library.
-    BVLIB_MINOR_VERSION = 0, ///< Minor version of the library.
+    BVLIB_MINOR_VERSION = 3, ///< Minor version of the library.
     BVLIB_MICRO_VERSION = 0  ///< Micro version of the library.
 };
 
